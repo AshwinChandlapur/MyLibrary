6c2785e7b6f81df194bc797bd9906cca90576c78
AshwinChandlapur <ashwinchandlapur@gmail.com>
2020-03-12 14:43:55 -0400

    Library asking permission

--- toasterlibary/src/main/java/vadeworks/toasterlibary/ToasterMessage.java
+++ toasterlibary/src/main/java/vadeworks/toasterlibary/ToasterMessage.java
@@ -2,16 +2,20 @@ package vadeworks.toasterlibary;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 public class ToasterMessage {
 
-    public static void s(Context c, String message) {
+    public static void s(Context c, String message, Activity a) {
 
         if (checkPermission(c)) {
             //main logic or main code
@@ -19,7 +23,7 @@ public class ToasterMessage {
             // . write your main code to execute, It will execute if the permission is already given.
 
         } else {
-            requestPermission(c);
+            requestPermission(a);
         }
 
 
@@ -35,9 +39,9 @@ public class ToasterMessage {
         return true;
     }
 
-    private static void requestPermission(Context c) {
+    private static void requestPermission(Activity a) {
 
-        ActivityCompat.requestPermissions((Activity) c,
+        ActivityCompat.requestPermissions(a,
                 new String[]{Manifest.permission.CAMERA},
                 200);
     }
